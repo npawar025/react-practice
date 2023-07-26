f689c4b840e20ea0a3409ef657364a75ba1a3b7c
Nilesh <npawar025@gmail.com>
2023-07-26 18:55:13 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child-one" }, [
+    React.createElement("h1", {}, "This is the h1"),
+    React.createElement("h2", {}, "This is the h2"),
+  ]),
+  React.createElement("div", { id: "child-two" }, [
+    React.createElement("h1", {}, "This is the h1"),
+    React.createElement("h2", {}, "This is the h2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
