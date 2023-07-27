2b0375fff0577739e24c96632af09459bb1d191b
Nilesh <npawar025@gmail.com>
2023-07-27 17:40:31 +0530

    third commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Title = () => {
+  return <h1>This is react title component</h1>;
+};
 
-
-const jsxHeading  = <h1>Namaste React by Nilesh Pawar</h1>;
+//React composition is nothing but nesting one component into the other component
+const ReactComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1>This is the react component</h1>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<ReactComponent />);
